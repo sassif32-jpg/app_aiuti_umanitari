f5c41a2f459cf507ef74733e073b9e92fe2b10ff
Francesca Sassi <sassif32@gmail.com>
2026-08-09 15:12:27 +0200

    Initial commit: aggiunta struttura base del'app con HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,26 @@
+function gestisciCalcoloAccqua() {
+    const persone = Number(document.getElementById('numPersone').value);
+    const giorni = Number(document.getElementById('numGiorni').value);
+    const divRisultato = document.getElementById('risultatoAcqua');
+
+    if (persone <= 0 || giorni <= 0) {
+        divRisultato.innerText = "Inserisci valori validi maggiori di zero.";
+    } else {
+        const totaleLitri = calcolaAcqua(persone, giorni);
+        divRisultato.innerText = `Fabbisogno totale: ${totaleLitri} Litri d'acqua.`;
+    }
+    divRisultato.Risultato.style.display = 'block';
+}
+
+function gestisciTriage() {
+    const temp = Number(document.getElementById('tempPaziente').value);
+    const divRisultato = document.getElementById('risultatoTriage');
+
+    if (!temp) {
+        divRisultato.innerText = "Inserisci un valore di tempertura valido.";
+    } else {
+        const stato = validaTemperatura(temp);
+        divRisultato.innerText = `Stato paziente: ${stato}`;
+    }
+    divRisultato.style.display = 'block';
+}
